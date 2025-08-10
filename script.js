6bf26772b41a4f95cf477c17e15ad368ffdd17b7
Nare <hnare4378@gmail.com>
2025-08-10 22:44:03 +0400

    changed the photo1

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Slideshow images (replace with your actual wedding photos)
     const images = [
-        'https://i.imgur.com/KqiDu94.jpeg',
+        'https://i.imgur.com/WNXWuf6.jpeg',
         'https://i.imgur.com/SHOLSpH.jpeg',
         'https://placehold.co/1920x1080/F2D7D5/FFFFFF?text=Mane+and+Robert+3'
     ];
